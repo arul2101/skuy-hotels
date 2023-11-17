d3a923f1a1f1c38774977526ab25701530c12ef9
arul2101 <myanuarullah20@gmail.com>
2023-11-17 14:01:34 +0700

    Fix bug in the App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ import Profile from "@/pages/Profile";
 import Bookings from "./pages/Bookings";
 import BookingDetail from "./pages/BookingDetail";
 import CheckIn from "./pages/CheckIn";
-import Notfound from "./components/notfound";
+import Notfound from "./components/NotFound";
 import CreateBooking from "./pages/CreateBooking";
 
 import ErrorFallback from "@/components/ErrorFallback";
